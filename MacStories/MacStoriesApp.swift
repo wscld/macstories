3cc2e30dee77d1087664ae01abf17df96aa0fa90
Wesley Caldas <wesleycaldas@MacBook-Air-de-Wesley.local>
2025-03-25 18:16:08 -0300

    Initial Commit

---
+++ MacStories/MacStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacStoriesApp.swift
+//  MacStories
+//
+//  Created by Wesley Caldas on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MacStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacStoriesTests/MacStoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacStoriesTests.swift
+//  MacStoriesTests
+//
+//  Created by Wesley Caldas on 25/03/25.
+//
+
+import Testing
+@testable import MacStories
+
+struct MacStoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
